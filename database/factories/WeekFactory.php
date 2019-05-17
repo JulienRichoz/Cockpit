883baf528fdab6d67ad4993618e21fd536b9632c
JulienRichoz <julienrichoz@outlook.com>
2019-05-17 11:46:43 +0200

    Modify class name of week factory

--- database/factories/WeekFactory.php
+++ database/factories/WeekFactory.php
@@ -2,10 +2,10 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Factory;
+use App\Week;
 use Faker\Generator as Faker;
 
-$factory->define(Factory::class, function (Faker $faker) {
+$factory->define(Week::class, function (Faker $faker) {
     return [
         'name' => $faker->catchPhrase(50),
         'location' => $faker->city(50),
