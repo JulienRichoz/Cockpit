e16aa9d8ad6e8b96e86aeeca0ed301b2ffbe83ce
JulienRichoz <julienrichoz@outlook.com>
2019-05-17 11:47:36 +0200

    Add manual seeding  (events/users)

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -11,6 +11,17 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // MANUAL SEEDER
+
+        /*
+            Generate one instruction
+        */ 
+        DB::table('events') 
+            -> insert(array(
+                'attention_point' => 'Travaux en cours à Los Angeles',
+                'project' => 'Défense de diplôme le 20.06',
+                'diverse' => 'Sortie au MAD le 20.06',
+                'calendar' => 'Bon anniversaire Julien (27.10) !',
+            ));
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // MANUAL SEEDER
+
+        /* Create two definite users
+            1. mail: admin@admin.com    username: admin  Password: admin
+            2. mail: test@test.com      username: test   Password: test
+        */
+        DB::table('users') 
+            -> insert(array(
+                // Admin user
+                array(
+                    'name' => 'admin',
+                    'username' => 'admin',
+                    'email' => 'admin@admin.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('admin'),
+                    'remember_token' => Str::random(10),
+                ),
+                // Test user
+                array(
+                    'name' => 'test',
+                    'username' => 'test',
+                    'email' => 'test@test.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('test'),
+                    'remember_token' => Str::random(10),
+                )
+            ));
     }
 }
