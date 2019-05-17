bf9447534bbdbc9e3d41b5bbb3364741cba00ca6
JulienRichoz <julienrichoz@outlook.com>
2019-05-17 11:46:29 +0200

    Correct syntax from a field

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -9,7 +9,7 @@ $factory->define(Activity::class, function (Faker $faker) {
     return [
         'type' => rand(1,2),
         'name' => $faker->catchPhrase(50),
-        'place' => $faker->city(50),
+        'location' => $faker->city(50),
         'start_date'=>$faker->dateTimeBetween('now', '+15 days'),
         'end_date'=>$faker->dateTimeBetween('+16 days', '+200 days'),
     ];
