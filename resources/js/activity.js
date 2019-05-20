074083c3658f161e315a1bec8ceb669c07048706
JulienRichoz <julienrichoz@outlook.com>
2019-05-20 16:30:01 +0200

    Add modal edit in activity view

--- resources/js/activity.js
+++ resources/js/activity.js
@@ -1,11 +1,25 @@
 import { callAjax } from './modules/callajax';
 import swal from 'sweetalert'
 
+// Edit an activity on click (modal popup)
+$('#activities_row .editable_row ._edit_').click(function(){
+    console.log("AHAHAHAHAHA");
 
-// SOFT DELETE ON CLICK AN ACTIVITY
+    let modal = $('#add_activity_modal');
+
+    // Get the values from the view in the modal to get the current data.
+    modal.find('#name').val($(this).parent().data('name'));
+    modal.find('#location').val($(this).parent().data('location'));
+    modal.find('#start_date').val($(this).parent().data('start_date'));
+    modal.find('#end_date').val($(this).parent().data('end_date'));
+    modal.find('#activity_id').val($(this).parent().data('id'));
+    modal.find('#activity_type_id').val($(this).parent().data('activity_type_id'));
+
+    $('#add_activity_modal').modal('toggle');
+});
+
+// Softdelete an activity on click
 $('#activities_row .delete_activity_cross').click(function(){
-
-    console.log('ss');
     const data = {
         id: $(this).parent().data('id'),
     };
