09e721baaf0316b3494716ce210f7ff431f84aa0
JulienRichoz <julienrichoz@outlook.com>
2019-05-20 11:07:35 +0200

    Add SoftDelete class in model

--- app/Activity.php
+++ app/Activity.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Activity extends Model
 {
+    use SoftDeletes;
     // The attributes that are mass assignable.
     protected $fillable = [
         'type', // enum
@@ -14,4 +16,20 @@ class Activity extends Model
         'begin_date', // date
         'end_date'  //date
     ];
+
+    /**
+     * Add constant parameters to the activtiy model
+     */
+    const ACTIVITY_TYPES = [
+        1 => ['name' => 'exploitation'],
+        2 => ['name' => 'operationnel']
+    ];
+
+    public function getType(){
+        return self::ACTIVITY_TYPES[$this->type];
+    }
+
+    public static function getTypes(){
+        return self::ACTIVITY_TYPES;
+    }
 }
