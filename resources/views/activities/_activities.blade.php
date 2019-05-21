4b2c8f843aa83d0130f7b434b0c61f3e2354de6e
JulienRichoz <julienrichoz@outlook.com>
2019-05-21 08:56:56 +0200

    Add control to allow creating 16 activities max

--- resources/views/activities/_activities.blade.php
+++ resources/views/activities/_activities.blade.php
@@ -22,7 +22,7 @@
                     <tr class="editable_row"
                         data-id="{{ $activity->id }}"
                         data-name="{{ $activity->name }}"
-                        data-location="{{ $activity->place }}"
+                        data-location="{{ $activity->location }}"
                         data-start_date="{{ $activity->start_date }}"
                         data-end_date="{{ $activity->end_date }}"
                         data-activity_type_id="{{ $activity }}"
@@ -31,7 +31,7 @@
                             <td class="delete_activity_cross" data-route="{{ route('archive_activity') }}"><i class="fas fa-times text-danger"></i></td>
                         @endauth
                         <th scope="row" class="activity_name _edit_">{{ $activity->name }}</th>
-                        <td class="activity_location _edit">{{ $activity->location }}</td>
+                        <td class="activity_location _edit_">{{ $activity->location }}</td>
                         <td class="activity_start_date _edit_" data-date="{{ $activity->start_date }}">{{ \Carbon\Carbon::createFromDate($activity->start_date)->format('d.m.Y') }}</td>
                         <td class="activity_end_date _edit_" data-date="{{ $activity->end_date }}">{{ \Carbon\Carbon::createFromDate($activity->end_date)->format('d.m.Y') }}</td>
                         @for($i=1; $i<=12; $i++)
@@ -39,6 +39,21 @@
                         @endfor
                     </tr>
                     @endforeach
+                    @auth
+                        @if($activities_count < 16)
+                            <tr class="editable_row"
+                                data-activity_type_id="{{ $activity_type_id }}">
+                                <td></td>
+                                <td scope="row" class="activity_name _edit_">&nbsp;</td>
+                                <td class="activity_location _edit_"></td>
+                                <td class="activity_start_date _edit_" data-date=""></td>
+                                <td class="activity_end_date _edit_" data-date=""></td>
+                                @for($i=1; $i<=12; $i++)
+                                    <td></td>
+                                @endfor
+                            </tr>
+                        @endif
+                    @endauth
                 </tbody>
             @endforeach
         </table>
