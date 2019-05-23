7365ae21e9241b622542c09dabef603a31e825e7
JulienRichoz <julienrichoz@outlook.com>
2019-05-23 17:00:44 +0200

    Add picket module view

--- resources/views/pickets/_pickets.blade.php
+++ resources/views/pickets/_pickets.blade.php
@@ -1,1 +1,67 @@
-<h1>PICKETS</h1>
+<div class="col-3 text-center" id="picket">
+    @if($pickets->count() == 0)
+        @guest
+            <span class="noPicket">
+                <i class="fa fa-exclamation-triangle fa-5x redIcon" aria-hidden="true"></i> 
+                <h2 class="redText"><b>Pas de piquets prévus</b></h2>
+            <span>
+        @endguest
+        
+        @auth
+            <a href="{{ route('edit_picket') }}"><i class="button fas fa-plus-circle fa-10x "></i></a>
+        @endauth
+    @else
+        <div class="row">
+            <!-- Piquet 1 semaine actuelle -->
+            <div class="col-md-6">
+                    <div class="card text-white bg-card-main mb-3" style="max-width: 18rem;">
+                        <div class="card-body">
+                            <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[0]->main) }}</h1>
+                        </div>
+                        <div class="card-footer bg-card-footer">
+                            Piquet 1
+                        </div>
+                    </div>
+                </div>
+
+            <!-- Piquet 1 semaine suivante -->
+            <div class="col-md-6">
+                <div class="card text-white bg-card-main mb-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[1]->main) }}</h1>
+                    </div>
+                    <div class="card-footer bg-card-footer">
+                        Piquet 1
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <!-- Piquet 2 semaine actuelle -->
+            <div class="col-md-6">
+                <div class="card text-white bg-card-main mb-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[0]->substitute) }}</h1>
+                    </div>
+                    <div class="card-footer bg-card-footer">
+                        Piquet 2
+                    </div>
+                </div>
+            </div>
+
+             <!-- Piquet 2 semaine suivante -->
+            <div class="col-md-6">
+                <div class="card text-white bg-card-main mb-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[1]->substitute) }}</h1>
+                    </div>
+                    <div class="card-footer bg-card-footer">
+                        Piquet 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+</div>
