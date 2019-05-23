8cc3fb3775697adb2108b8a73cc178ba76d1abcd
JulienRichoz <julienrichoz@outlook.com>
2019-05-23 17:00:05 +0200

    Add menu to list picket for auth people

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                Dashboard
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
@@ -34,9 +34,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
-
                 </ul>
 
+                <ul class="navbar-nav mm-auto">
+                        @auth
+                        <a class="navbar-brand" href="{{ route('edit_picket') }}">Liste des piquets</a>   
+                    @endauth
+                </ul>
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
                     @guest
