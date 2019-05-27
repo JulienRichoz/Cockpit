2b6613f482b9b392b9d4ae6d447113b21ef56caa
JulienRichoz <julienrichoz@outlook.com>
2019-05-27 15:45:57 +0200

    Add method to convert date to percent

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Activity;
+use Carbon\Carbon;
 
 class ActivityController extends Controller
 {
@@ -81,5 +82,35 @@ class ActivityController extends Controller
                 'errors' => $e->getMessage()
             ]);
         }
+    }
+
+    /**
+     * Convert input date to a % of the year, to display it in a progress bar
+     */
+    public static function dateToPercent($date){
+
+        // Check if the input date is valid
+        try{
+            Carbon::parse($date);
+        } catch (\Exception $e){
+            return 'Invalid date';
+        }
+        $date = Carbon::parse($date);
+        // We handle only date from the current year, else we don't display it
+        $currentDate = Carbon::now();
+        if($date->year == $currentDate->year && $date){    
+            // Convert the input date in the number of days from current year
+            $firstDayOfYear = Carbon::parse((string) $currentDate->year.'-01-01');  // Convert the first day of the year in carbon date format
+            $dateToDays = $date->diffInDays($firstDayOfYear)+1; // Convert the input date to days number in this year
+            $daysInThisYear = 365 + $firstDayOfYear->format('L');   // Get the number of days in this year (including leap year). $firstDayOfYear returns 1 if it's a leap year otherwise 0
+
+            // Convert input date to % and round it to 2 decimal 
+            return round($dateToDays/$daysInThisYear * 100, 2);
+        }
+        
+        // Return 367 if the date to display is > currentYear
+        if($date->year > $currentDate->year) return 367;
+
+        return false;
     }
 }
