1d835ce2f9bef418316795d5193f3fa132fdcc43
JulienRichoz <julienrichoz@outlook.com>
2019-05-28 16:11:58 +0200

    Better display of picket with button click auth

--- resources/views/pickets/_pickets.blade.php
+++ resources/views/pickets/_pickets.blade.php
@@ -1,44 +1,63 @@
 <div class="col-3 text-center" id="picket">
-    @if($pickets->count() == 0)
+    @if($pickets->count() == 0)    
         @guest
             <span class="noPicket">
                 <i class="fa fa-exclamation-triangle fa-5x redIcon" aria-hidden="true"></i> 
-                <h2 class="redText"><b>Pas de piquets prévus</b></h2>
+                <h2 class="redText"><b>Pas de piquets en cours</b></h2>
             <span>
         @endguest
         
         @auth
-            <a href="{{ route('edit_picket') }}"><i class="button fas fa-plus-circle fa-10x"></i></a>
+            <div class="row justify-content-center">
+                <a href="{{ route('edit_picket') }}">
+                    <i class="button fas fa-plus-circle fa-10x"></i>
+                </a>
+            </div>
         @endauth
+        @if($next_picket)
+            <span class="redText">Prochain piquet prévu le {{ (Date::CreateFromDate($next_picket->start_date)->format('l d.m à H\hi')) }}</span>
+        @endif
     @else
     <div class="row">
 
         <!-- SEMAINE ACTUELLE -->
-        <div class="col-6  border-right border-secondary">
+        <div class="col-6  d-flex align-items-stretch border-right border-secondary">
             <div class="row" >
 
                 <!-- PIQUET 1 -->
                 <div class="col-12 ">
-                    <div class="card mx-auto text-white bg-card-main mb-3">
+                    @auth
+                        <a style="text-decoration: none;" href="{{ route('edit_picket') }}">
+                    @endauth
+                    <div class="card mx-auto text-white bg-card-main mb-2">
                         <div class="card-body">
-                            <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[0]->main) }}</h1>
+                            <span class="card-title" style="font-size: 200%;">{{ App\Picket::getPicketShortName($pickets[0]->main) }}</span>
                         </div>
                         <div class="card-footer bg-card-footer">
                             Piquet 1
                         </div>
                     </div>
+                    @auth
+                        </a>
+                    @endauth
                 </div>
 
                 <!-- PIQUET 2 -->
                 <div class="col-12">
-                    <div class="card mx-auto text-white bg-card-main mb-3">
+                    @auth
+                        <a style="text-decoration: none;" href="{{ route('edit_picket') }}">
+                    @endauth
+                    <div class="card mx-auto text-white bg-card-main mb-2">
                         <div class="card-body">
-                            <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[0]->substitute) }}</h1>
+                            <span class="card-title" style="font-size: 200%;">{{ App\Picket::getPicketShortName($pickets[0]->substitute) }}</h2>
                         </div>
                         <div class="card-footer bg-card-footer">
                             Piquet 2
                         </div>
                     </div>
+                    @auth 
+                        </a> 
+                    @endauth
                 </div> 
 
                 <!-- DATE PIQUETS -->
@@ -69,37 +88,52 @@
         </div>
         @else
             <!-- SEMAINE SUIVANTE -->
-            <div class="col-6 border-left border-secondary">
+            <div class="col-6 d-flex align-items-stretch border-left border-secondary">
                 <div class="row">
 
                     <!-- PIQUET 1 -->
                     <div class="col-12">
+                        @auth
+                            <a style="text-decoration: none;" href="{{ route('edit_picket') }}">
+                        @endauth
                         <div class="card mx-auto text-white bg-card-main mb-3">
                             <div class="card-body">
-                                <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[1]->main) }}</h1>
+                                <span class="card-title" style="font-size: 200%;">{{ App\Picket::getPicketShortName($pickets[1]->main) }}</h2>
                             </div>
-                            <div class="card-footer bg-card-footer">
+                            <div class="card-footer bg-card-footer h-10">
                                 Piquet 1
                             </div>
                         </div>
+                        @auth
+                            </a>   
+                        @endauth
                     </div>
             
                     <!-- PIQUET 2 -->
                     <div class="col-12">
+                        @auth
+                            <a style="text-decoration: none;" href="{{ route('edit_picket') }}">
+                        @endauth
                         <div class="card mx-auto text-white bg-card-main mb-3">
                             <div class="card-body">
-                                <h1 class="card-title">{{ App\Picket::getPicketShortName($pickets[1]->substitute) }}</h1>
+                                <span class="card-title" style="font-size: 200%;">{{ App\Picket::getPicketShortName($pickets[1]->substitute) }}</h2>
                             </div>
                             <div class="card-footer bg-card-footer">
                                 Piquet 2
                             </div>
                         </div>
+                        @auth
+                            </a>   
+                        @endauth
                     </div>
 
                     <!-- DATE PIQUETS -->
                     <div class="col-12">
                         @if($gap_time_pickets >= 1)
-                            <span>{{ Date::CreateFromDate($pickets[1]->end_date) }}</span> 
+                        <span class="relief-date">
+                            <i class="fas fa-arrow-right" style="font-size: 12px;"></i>
+                            &nbsp;{{ ucfirst(Date::CreateFromDate($pickets[1]->end_date)->format('D d.m à H\hi')) }}
+                        </span> 
                             <div class="row justify-content-center text-danger">
                                 @auth 
                                     <a class="text-danger" href="{{ route('edit_picket') }}"><b> Modifier relève ({{ $gap_time_pickets }} j.)  </b> </a> 
